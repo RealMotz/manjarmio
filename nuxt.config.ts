21a0442a4c71057737e70cc2f4557353b9707094
Motta <carlos.motta.business@gmail.com>
2026-07-22 19:45:38 -0500

    chore: add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: [],
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  runtimeConfig: {
+    // server only
+    public: {
+      pickupAddress: process.env.PICKUP_ADDRESS,
+      pickupHours: process.env.PICKUP_HOURS,
+    },
+  },
+});
